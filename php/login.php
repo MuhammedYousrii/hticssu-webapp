ad695de8a431b45fe6053c6bc4d13ce8fcee1003
MuhammeD YuosrY <MuhammeD YuosrY>
2017-04-22 00:03:29 +0200

    Enhacing DashBoard Login Stystem

--- php/login.php
+++ php/login.php
@@ -1,43 +1,69 @@
 <?php header('Access-Control-Allow-Origin: *');  ?>
 <?php
 
-//
-//define('DB_SERVER', 'server180');
-//define('DB_USERNAME', 'media_hticsuser');
-//define('DB_PASSWORD', 'Ips^biBk,Nn_');
-//define('DB_CHARSET', 'UTF8');
-//define('DB_DATABASE', 'media_htics');
-//define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE . ';charset=' . DB_CHARSET);
-//
-//$db = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
 
+define('DB_SERVER', 'server180');
+define('DB_USERNAME', 'media_hticsuser');
+define('DB_PASSWORD', 'Ips^biBk,Nn_');
+define('DB_CHARSET', 'UTF8');
+define('DB_DATABASE', 'media_htics');
+define('PDO_DSN', 'mysql:host=' . DB_SERVER . ';dbname=' . DB_DATABASE . ';charset=' . DB_CHARSET);
 
-$user_name_one = 'ahmedMonty';
-$user_password_one = 'pgk32gr6/Y=A~Yd2,k}';
-
-
-$user_name_two = 'aya-hesham';
-$user_password_two = 'K2KkJGPccmk9VMUnCV';
-
-
-$user_name_three = 'muhammed-yousrii';
-$user_password_three = 'muhammed225136578';
-
-
-$user_name_four = 'muhammed-reda';
-$user_password_four = 'HC52s4qQ8JENRmCfE7';
-
-$username = $_POST['username'];
-$password = $_POST['password'];
-
-if ($username == $user_name_one && $password == $user_password_one ||
-    $username = $user_name_two && $password == $user_password_two ||
-    $username = $user_name_three && $password == $user_password_three ||
-    $username = $user_name_four && $password == $user_password_four ){
-    echo 'Okii' ;
-}else {
-    echo 'Wrong';
+$conn = new PDO(PDO_DSN, DB_USERNAME, DB_PASSWORD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES UTF8"));
+$NullIsset = "You didn't Pass Any Value In Form";
+$queryError = "Error Happen When Make Fetch On Tabels at DB";
+$not_Found = "UserName Or Password Didn't exisit ";
+function check_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = strip_tags($data);
+        $data = htmlspecialchars($data);
+        return $data;
 }
 
+
+
+if(isset($_GET['username'])){
+    if(empty($_GET['username'])){
+        echo $NullIsset; 
+    }else {
+        $username = check_input($_GET['username']);
+    }
+}
+if(isset($_GET['password'])){
+    if(empty($_GET['password'])){
+        echo $NullIsset; 
+    }else {
+    $password = $_GET['password'] ;
+    }
+}
+
+
+
+
+if($conn){
+    $query = $conn->prepare("SELECT `user_name`, `user_image`, `user_password`, `people_case` , `user_gender` FROM `people` WHERE user_password = '$password' ");
+    if($query){
+        $query->execute();
+        $row = $query->fetch(PDO::FETCH_ASSOC);
+        if($row['user_password'] == $password){
+            echo json_encode($row);
+        }
+        else {
+            echo json_encode($not_found);
+        }
+        
+        
+    }else{
+        echo $queryError ;
+    }
+}
+else {
+    echo 'Error happen When Connecting With DataBase' ;
+    var_dump($e->getMessage());
+}
+    
+    
+        
 
 ?>
